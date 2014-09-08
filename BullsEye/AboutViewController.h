b40c851696b8531c243af674b2286456d8245936
Jun Hong Park <JP@wl-215-37.wpa.claremont.edu>
2014-09-08 15:32:20 -0700

    Wenderlich tutorial completed up to page 100

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Jun Hong Park on 9/8/14.
+//  Copyright (c) 2014 John Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
